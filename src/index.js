bdfa0fade1dd0a684fe78e78132f67be8b5bcf08
Lucas K Allmon <lucaskallmon@gmail.com>
2017-12-19 14:31:35 -0800

    fix(npm): post build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Button from '../packages/Button';
+import Button from '../packages/Button/src/Button';
 import ProgressBar from '../packages/ProgressBar/ProgressBar';
 import SplitButton from '../packages/SplitButton/SplitButton';
 import Tag from '../packages/Tag/Tag';
