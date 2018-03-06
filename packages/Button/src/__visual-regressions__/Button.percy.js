11dadd6ba4c1048a25230b557a5ed71fe14f68cc
Lucas K Allmon <lucaskallmon@gmail.com>
2018-03-06 16:43:55 -0700

    fix(breakpoints): add breakpoints to test suite (#7)

--- packages/Button/src/__visual-regressions__/Button.percy.js
+++ packages/Button/src/__visual-regressions__/Button.percy.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '../Button';
-import css from '../../../../node_modules/tachyons/css/tachyons.min.css';
+import { breakpoints } from '../../../../percy.config.js';
+import css from 'tachyons'; //eslint-disable-line
 
-percySnapshot('Button Default', () => <Button>Click Me</Button>);
-percySnapshot('Button Disabled', () => <Button disabled>Click Me</Button>);
-percySnapshot('Button Radius 1', () => <Button radius={1}>Click Me</Button>);
-percySnapshot('Button Radius 2', () => <Button radius={2}>Click Me</Button>);
-percySnapshot('Button Radius 3', () => <Button radius={3}>Click Me</Button>);
-percySnapshot('Button Pill', () => <Button pill>Click Me</Button>);
+suite('Button', { widths: breakpoints }, () => {
+  percySnapshot('Default', () => <Button>Click Me</Button>);
+  percySnapshot('Disabled', () => <Button disabled>Click Me</Button>);
+  percySnapshot('Radius 1', () => <Button radius={1}>Click Me</Button>);
+  percySnapshot('Radius 2', () => <Button radius={2}>Click Me</Button>);
+  percySnapshot('Radius 3', () => <Button radius={3}>Click Me</Button>);
+  percySnapshot('Pill', () => <Button pill>Click Me</Button>);
+});
