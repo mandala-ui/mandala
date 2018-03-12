44cc119384ab520b7efe691aa331872ec0247d25
Lucas K Allmon <lucaskallmon@gmail.com>
2018-03-12 15:05:25 -0700

    chore(dependencies): update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export Button from '@mandala-ui/button';
+export List from '@mandala-ui/list';
 export ProgressBar from '@mandala-ui/progress-bar';
 export SplitButton from '@mandala-ui/split-button';
 export Tag from '@mandala-ui/tag';
