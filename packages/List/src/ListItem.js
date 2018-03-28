ca445744fb778df2530adc29f9f7348c9f0dc353
Lucas K Allmon <lucaskallmon@gmail.com>
2018-03-28 14:39:53 -0700

    feature(list): more robust list

--- packages/List/src/ListItem.js
+++ packages/List/src/ListItem.js
@@ -3,10 +3,18 @@ import PropTypes from 'prop-types';
 
 class ListItem extends PureComponent {
   render() {
-    const { borderColor, borderWidth, children, hasBorder } = this.props;
-    const borders = `${borderWidth === 0 ? '' : `bw${borderWidth}`} ${hasBorder ? `bb b--${borderColor}` : 'bb b--transparent'}`;
+    const {
+      borderColor,
+      borderWidth,
+      children,
+      hasBorder,
+      indented,
+    } = this.props;
+    const borders = `${borderWidth === 0 ? '' : `bw${borderWidth}`}
+                    ${hasBorder ? `bb b--${borderColor}` : 'bb b--transparent'}`;
+    const indents = `${indented ? 'ph3' : ''}`;
     return (
-      <li className={`list-item ${borders}`}>
+      <li className={`list-item pv3 ${indents} ${borders}`}>
         {children || null}
       </li>
     );
@@ -16,15 +24,17 @@ class ListItem extends PureComponent {
 ListItem.propTypes = {
   borderColor: PropTypes.string,
   borderWidth: PropTypes.number,
-  hasBorder: PropTypes.bool,
   children: PropTypes.node,
+  hasBorder: PropTypes.bool,
+  indented: PropTypes.bool,
 };
 
 ListItem.defaultProps = {
-  borderColor: 'gray',
+  borderColor: 'black',
   borderWidth: 0,
   children: null,
   hasBorder: false,
+  indented: false,
 };
 
 export default ListItem;
