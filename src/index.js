137b1f6fc66ca112184e2728b8070873c3ba146c
Lucas K Allmon <lucaskallmon@gmail.com>
2018-03-29 09:52:41 -0700

    publish(1.2.6): add list-header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export Button from '@mandala-ui/button';
 export Column from '@mandala-ui/column';
 export List from '@mandala-ui/list';
+export ListHeader from '@mandala-ui/list-header';
 export OutlineDot from '@mandala-ui/outline-dot';
 export ProgressBar from '@mandala-ui/progress-bar';
 export SplitButton from '@mandala-ui/split-button';
