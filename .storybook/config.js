159b15f88b3bcc5a926d8427169a18d3ae14d5e6
Lucas K Allmon <lucaskallmon@gmail.com>
2018-04-06 09:50:54 -0700

    feature(stories): generator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,18 @@
+import React from 'react';
 import { configure } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
+import { addDecorator } from '@storybook/react';
+import tachyons from 'tachyons';
+
+const CenterPadding = storyFn => (
+  <div className="pa5 sans-serif">
+    <div className="w-100 flex justify-center">
+      { storyFn() }
+    </div>
+  </div>
+);
+
+addDecorator(CenterPadding)
 
 setOptions({
   name: 'Mandala UI',
@@ -12,4 +25,8 @@ setOptions({
   sortStoriesByKind: true,
 });
 
-configure(() => require('../stories'), module); // eslint-disable-line
+const req = require.context('../stories', true, /\.story\.js$/)
+
+configure(() => {
+  req.keys().forEach((filename) => req(filename))
+}, module);
