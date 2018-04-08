34a9f60442940d0c77d39ced38b4191107fac0aa
Lucas K Allmon <lucaskallmon@gmail.com>
2018-04-08 14:28:09 -0700

    feature(plop): different types of component generators

--- plopfile.js
+++ plopfile.js
@@ -1,85 +1,50 @@
-/* eslint-disable */
-module.exports = function (plop) {
-    plop.setPartial('injectPascalCaseName', '{{properCase name}}');
-    plop.setPartial('injectDashCaseName', '{{dashCase name}}');
-    plop.setGenerator('Pure Component', {
-        description: 'scaffolds a functional component',
-        prompts: [
-          {
-            type: 'input',
-            name: 'name',
-            message: 'Component Name (use PascalCase):',
-            validate: function (value) {
-              if ((/^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/).test(value)) { return true; }
-              return 'Name needs to be PascalCase (I told you this...)';
-            },
-          },
-          {
-            type: 'confirm',
-            name: 'usesEvents',
-            message: 'Does your component use callbacks with events? (Storybook Action Logger)'
-          },
-          {
-            type: 'checkbox',
-            name: 'knobs',
-            message: 'Which prop types (Storybook Knobs) will you need?',
-            choices: [
-              { name: 'String',
-                value: 'text',
-              },
-              { name: 'Boolean',
-                value: 'boolean'
-              },
-              { name: 'Number',
-                value: 'number'
-              },
-              { name: 'Object',
-                value: 'object',
-              },
-              { name: 'Array',
-                value: 'array',
-              },
-              { name: 'Range (Number Slider)',
-                value: 'range'
-              },
-            ]
-          }
-        ],
-        actions: [{
-            type: 'add',
-            path: 'packages/{{properCase name}}/src/{{properCase name}}.js',
-            templateFile: '.generators/functionalcomponent.hbs'
-        },
+const union = require('lodash/union');
+const prompts = require('./.generators/shared-prompts.js');
+const actions = require('./.generators/shared-actions.js');
+
+module.exports = function generate(plop) {
+  plop.setPartial('injectPascalCaseName', '{{properCase name}}');
+  plop.setPartial('injectDashCaseName', '{{dashCase name}}');
+  plop.setGenerator('Functional Component', {
+    description: 'scaffolds a function that returns JSX',
+    prompts,
+    actions: union(
+      actions,
+      [
         {
           type: 'add',
-          path: 'packages/{{properCase name}}/src/__tests__/{{properCase name}}.test.js',
-          templateFile: '.generators/test.hbs'
+          path: 'packages/{{properCase name}}/src/{{properCase name}}.js',
+          templateFile: '.generators/functional-component.hbs',
         },
+      ],
+    ),
+  });
+  plop.setGenerator('Pure Component', {
+    description: 'scaffolds a React PureComponent',
+    prompts,
+    actions: union(
+      actions,
+      [
         {
           type: 'add',
-          path: 'stories/{{properCase name}}.story.js',
-          templateFile: '.generators/story.hbs'
+          path: 'packages/{{properCase name}}/src/{{properCase name}}.js',
+          templateFile: '.generators/pure-component.hbs',
         },
+      ],
+    ),
+  });
+  plop.setGenerator('Stateful Component', {
+    description: 'scaffolds a stateful React Component',
+    prompts,
+    actions: union(
+      actions,
+      [
         {
           type: 'add',
-          path: 'packages/{{properCase name}}/src/__visual-regressions__/{{properCase name}}.percy.js',
-          templateFile: '.generators/visual-regressions.hbs'
+          path: 'packages/{{properCase name}}/src/{{properCase name}}.js',
+          templateFile: '.generators/stateful-component.hbs',
         },
-        {
-            type: 'add',
-            path: 'packages/{{properCase name}}/LICENSE.md',
-            templateFile: '.generators/LICENSE.hbs'
-        },
-        {
-            type: 'add',
-            path: 'packages/{{properCase name}}/package.json',
-            templateFile: '.generators/package.hbs'
-        },
-        {
-            type: 'add',
-            path: 'packages/{{properCase name}}/README.md',
-            templateFile: '.generators/README.hbs'
-        },
-      ]
-    });
+      ],
+    ),
+  });
 };
